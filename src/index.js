a03eb5df3c200bd985077e0a1f263f1fdd81a32a
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-22 15:30:44 -0500

    add cards function

--- src/index.js
+++ src/index.js
@@ -19,10 +19,16 @@ ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
     <Card 
-    name="Beyonce" 
+    name="cat in hat" 
     img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
     tel="12345678" 
-    email="b@beyonce.com"
+    email="catinhat@gmail.com"
+    />
+    <Card 
+    name="Jerry" 
+    img="https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png" 
+    tel="12345679" 
+    email="jerry@gmailcom"
     />
   </div>,
   document.getElementById("root")
